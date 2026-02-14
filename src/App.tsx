5b4fc4a646000e762aaa4f91c9dab160fdb7d448
Sara <sarailic431@gmail.com>
2026-02-14 17:27:48 +0100

    initial commit starting ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <form className="w-[fit-content] !mx-auto flex flex-col">
+        <input placeholder="Email" type="text" />
+        <input placeholder="Password" type="password" />
+      </form>
+    </>
+  );
 }
 
 export default App;
