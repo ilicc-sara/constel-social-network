5c73c7f1ef66b2336a14c537000b7cbaef2e9038
Sara <sarailic431@gmail.com>
2026-02-16 20:32:10 +0100

    razradjujem brale

---
+++ src/LogIn.tsx
@@ -0,0 +1,31 @@
+function LogIn() {
+  return (
+    <section className="min-h-screen !mt-[5%]">
+      <div className="w-[fit-content] !mx-auto flex justify-center items-center gap-2">
+        <img className="w-12 h-12" src="logo.jpg" />
+      </div>
+
+      <form className="w-104 h-[fit-content] !px-14 !py-9 flex flex-col bg-[#2222] !mx-auto items-start justify-start gap-5 rounded-xl !my-5">
+        <div className="flex flex-col w-full">
+          Email
+          <input
+            className="bg-white w-full pl-2"
+            placeholder="Enter your email here..."
+            type="text"
+          />
+        </div>
+
+        <div className="flex flex-col w-full">
+          Password
+          <input
+            className="bg-white w-full pl-2"
+            placeholder="Enter your password here..."
+            type="password"
+          />
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default LogIn;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import LogIn from "./LogIN";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LogIn />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
