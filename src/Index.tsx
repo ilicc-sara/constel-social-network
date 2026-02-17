4147514008492ffd06a6dee4978123ce4fa73c17
Sara <sarailic431@gmail.com>
2026-02-17 18:52:31 +0100

    ....

--- src/Index.tsx
+++ src/Index.tsx
@@ -49,7 +49,7 @@ function Home() {
   return (
     <section className="min-h-screen !mt-[5%] flex flex-col items-start gap-5">
       <div className="w-[600px] !mx-auto ">
-        <form className="flex flex-col bg-gray-200 !p-5">
+        <form className="flex flex-col bg-gray-200 !p-5 rounded">
           <div className="flex">
             <figure className="!mr-4 w-15 aspect-square">
               <img
@@ -71,28 +71,34 @@ function Home() {
         </form>
       </div>
 
-      <article className="w-[600px] !mx-auto ">
-        <div className="flex flex-col bg-gray-200 !p-4">
-          <div className="flex items-start">
-            <figure className="!mr-4 w-15 aspect-square">
-              <img
-                src="profile-pic.jpg"
-                className="w-full h-full ovject-cover rounded-full"
-              />
-            </figure>
-            <div>
-              <p>@dzek_ludoriba</p>
-              <p>Jack Ludoriba</p>
-            </div>
-            <div className="text-right !ml-auto">
-              <span>09.08.2024.</span>
-            </div>
+      <article className="w-[600px] !mx-auto flex flex-col bg-gray-200 !p-4 gap-3 rounded">
+        <div className="flex items-start">
+          <figure className="!mr-4 w-15 aspect-square">
+            <img
+              src="profile-pic.jpg"
+              className="w-full h-full ovject-cover rounded-full"
+            />
+          </figure>
+          <div>
+            <p>@dzek_ludoriba</p>
+            <p>Jack Ludoriba</p>
           </div>
-          <div className="flex justify-end !my-3">
-            <button className="bg-blue-500 text-white !py-2 !px-4 rounded">
-              New Post
-            </button>
+          <div className="text-right !ml-auto">
+            <span>09.08.2024.</span>
           </div>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
+            delectus soluta tenetur inventore, at doloremque nulla ratione aut
+            facere nostrum explicabo necessitatibus blanditiis quibusdam
+            architecto, repellendus temporibus praesentium est sapiente!
+          </p>
+          <img src="clif.jpg" className="w-full h-full ovject-cover rounded" />
+        </div>
+        <div className="flex justify-start gap-5 !my-3">
+          <button className="bg-gray-300 rouded !px-3">Like</button>
+          <button className="bg-gray-300 rouded !px-3">Comment</button>
         </div>
       </article>
     </section>
